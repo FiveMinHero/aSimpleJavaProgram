788a73035bfb8d0e537f84475d04404ea395dc7c
hines3260@gmail.com <>
2019-02-06 16:20:11 -0500

    This is the first commit

---
+++ src/asimplejavaprogram/ASimpleJavaProgram.java
@@ -0,0 +1,15 @@
+
+package asimplejavaprogram;
+
+public class ASimpleJavaProgram {
+
+    public static void main(String[] args) 
+    {
+        Anderson();
+    }
+    public static void Anderson()
+    {
+        System.out.println("Cage Anderson!!!");
+    }
+    
+}
